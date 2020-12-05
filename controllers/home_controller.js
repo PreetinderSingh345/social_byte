00523c4ca615221ffc5f3a0e8b989f0b9ebac054
Preetinder Singh <preetindersingh072@gmail.com>
2020-12-05 20:07:12 +0530

    displayed the posts and their authors on the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
+const Post=require("../models/post");//requiring the Post model for which the postSchema has been defined
+
 module.exports.home=function(req, res){//home controller function/action and we are exporting it, so that it can be accesssed in the router section/folder
 
     // ** the below part where we accessed and altered the sample cookie data created from browser side is commented(cleared the sample cookie data from the browser side too) as we don't want the sample cookie data to interfere with the cookie data created by passport **
@@ -11,8 +13,26 @@ module.exports.home=function(req, res){//home controller function/action and we 
     // res.cookie("user_name", "ghijk");//initial value of "user_name" key was - "abcde"
     // res.cookie("user_id", 67890);//initial value of "user_id" key was - "12345"
 
-    return res.render("home", {//rendering the home page and providing the dynamic content
-        title: "Home"
-    });
+    // finding all the posts
+
+    Post.find({}).populate("user").exec(function(err, posts){//finding all the posts(all the posts will be returned as we have not passed any search criteria inside {}) and we are populating the user field of each found post to get the user object from its id(pre-populating the user) and we have a callback function inside exec() to handle the situation
+
+        if(err){//if there is an error while finding the posts
+
+            console.log(`Error in fetching the posts : ${err}`);//we print a relevant error message and simply return
+            return ;
+
+        }
+
+        // if the posts have been found successfully
+
+        return res.render("home", {//rendering the home page as the response
+
+            title: "Home",//providing the dynamic title and posts object
+            posts: posts
+
+        });
+
+    });    
 
 };
