d9b6a231d7b8723b133037fabf840e039f6ec700
Preetinder Singh <preetindersingh072@gmail.com>
2020-12-06 12:48:02 +0530

    making sure that a user can post only when it is signed in

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 const express=require("express");//requiring express and its existing instance will be used
 const router=express.Router();//getting the router
 const postsController=require("../controllers/posts_controller");//getting the posts controller
+const passport=require("passport");//requiring passport
 
-router.post("/create", postsController.create);//for handling the requests at "/create" route, we call the create action of the postsController
+router.post("/create", passport.checkAuthentication, postsController.create);//for handling the requests at "/create" route, we call the create action of the postsController and we're using checkAuthentication as a middleware for making sure a user can post only when it is signed in(this extra check is added so that the user is directed to the sign in page in case the user tries to send a post by altering the home page's HTML) 
 
 module.exports=router;//exporting the router, so that it can be accessed by the server to handle the incoming requests
