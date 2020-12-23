4bc8191e3d431ea8b2272aaab438599cd994abc2
Preetinder Singh <preetindersingh072@gmail.com>
2020-12-23 16:49:09 +0530

    setup new comment template for the comment mail being sent to the user

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,16 +1,17 @@
 const nodeMailer=require("../config/nodemailer");//requiring the nodemailer configuration, setup inside the config folder
 
-// defining and exporting new comment function which takes the comment made by a user as the argument and uses the sendMail function of nodemailer's transporter to send an email to the user, we provide to it the from, to, subject and the html content of the email and there is a callback consisting of error and information as the arguments and we print an error message, in case the mail couldn't be sent
+// defining and exporting new comment function which takes the comment made by a user as the argument, inside it we get the html string for the email to be sent using the renderTemplate function of nodeMailer, we provide to it the comment and the relative path, we use the sendMail function of nodemailer's transporter to send an email to the user, we provide to it the from, to, subject and the html content of the email(required inside htmlString) and there is a callback consisting of error and information as the arguments and we print an error message, in case the mail couldn't be sent
 
 exports.newComment=(comment)=>{
+
+    let htmlString=nodeMailer.renderTemplate({comment: comment}, "/comments/new_comment.ejs");
 
     nodeMailer.transporter.sendMail({
 
         from: "demo12345user67890@gmail.com",
         to: comment.user.email,
         subject: "New comment published",
-        html: `<h1>Comment - ${comment.content}</h1>
-        <h2> made on post - ${comment.post.content} `
+        html: htmlString
 
     }, (err, info)=>{
 
